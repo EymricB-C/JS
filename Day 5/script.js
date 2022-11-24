33ac5651c2692878087a09533f6a8a92fe49c160
EymricB-C <eymric.belloir@gmail.com>
2022-11-24 16:33:21 +0100

    modified:   Day 5/script.js

--- Day 5/script.js
+++ Day 5/script.js
@@ -40,9 +40,35 @@ function Personnage(name, type, force, PV) {
     };
 };
 
-let paladin1 = new Personnage("darion", "paladin", 3000, 14000);
-let tank1 = new Personnage("megan", "tank", 500, 50000);
+function Exp(exp, name, type, force, PV){
+    Personnage.call(this, name, type, force, PV)
 
-paladin1.attaque(tank1);
+    this.exp = exp
 
-tank1.attaque(paladin1);
+    if(exp < 1000){
+        level = "junior"
+    } else {
+        level = "senior"
+    }
+    console.log(name + " est niveau " + level)
+}
+
+function SuperSummon(exp, name, type, force, PV, bonus){
+    Exp.call(this, exp, name, type, force, PV)
+
+    this.showBonus = function (){
+        console.log(name +" a le bonus " + bonus)
+    }
+}
+
+const god = new SuperSummon(1000000, "god", "god", 1000000, 10000000, "fly")
+
+let Darion = new Personnage("darion", "paladin", 3000, 14000);
+let Bear = new Personnage("bear", "tank", 500, 100000);
+let spectra = new Exp(142, "spectra", "support", 750, 40000)
+
+god.showBonus();
+
+Darion.attaque(Bear);
+
+Bear.attaque(Darion);
