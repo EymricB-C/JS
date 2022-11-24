68d7013618ef1037b01f36bc492a8c36a872cf89
EymricB-C <eymric.belloir@gmail.com>
2022-11-24 11:12:41 +0100

    modified:   Day 5/script.js

--- Day 5/script.js
+++ Day 5/script.js
@@ -3,4 +3,21 @@ function Personnage(nom, type, force, vitalité) {
     this.type = type
     this.force = force
     this.vitalité = vitalité
+
+    this.attaque = function () {
+        console.log(nom + " fait " + force + " dégats")
+    }
+
+    this.sante = function () {
+        console.log(nom +" à " + vitalité + " PV")
+    }
 }
+
+const paladin1 = new Personnage("darion", "paladin", 200, 400)
+const sorcerer1 = new Personnage("megan", "sorcerer", 100, 200)
+
+paladin1.attaque();
+paladin1.sante();
+
+sorcerer1.attaque();
+sorcerer1.sante();
