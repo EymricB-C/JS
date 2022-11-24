1572a2a3c035d8d7bbef0213c211f387cac1a973
EymricB-C <eymric.belloir@gmail.com>
2022-11-24 13:39:39 +0100

    modified:   Day 5/script.js

--- Day 5/script.js
+++ Day 5/script.js
@@ -4,28 +4,27 @@ function Personnage(nom, type, force, PV) {
     this.force = force
     this.PV = PV
 
-    
-
     this.attaque = function (adversaire) {
-        console.log(nom + " fait " + force + " dégats sur " + adversaire.nom);
-        adversaire.PV = adversaire.PV - force;
+        if(this.PV>0) {
+            console.log(nom + " fait " + force + " dégats sur " + adversaire.nom);
+            adversaire.PV = adversaire.PV - force;
+            adversaire.sante();
+        }
     }
 
     this.sante = function () {
-        if(PV > 0){
-            console.log(nom +" à " + PV + " PV");
+        if(this.PV > 0){
+            console.log(nom +" à " + this.PV + " PV");
         }else{
-            console.log("personnage mort (comme ma maman)");
+            console.log(nom + " est mort(e)");
         }
         
     }
 }
 
-let paladin1 = new Personnage("darion", "paladin", 100, 400)
+let paladin1 = new Personnage("darion", "paladin", 200, 400)
 let sorcerer1 = new Personnage("megan", "sorcerer", 100, 200)
 
 paladin1.attaque(sorcerer1);
-paladin1.sante();
 
 sorcerer1.attaque(paladin1);
-sorcerer1.sante();
